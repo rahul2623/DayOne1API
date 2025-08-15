1da2cb7eadcae38f35eabe32b5f9b32d3cb2cb7e
rahul <rahul.arya7060762454@gmail.com>
2025-08-15 23:22:39 +0530

    two end points done

--- src/main/java/Controller/UserController.java
+++ src/main/java/Controller/UserController.java
@@ -5,25 +5,33 @@ import Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+import java.util.List;
 
-@RequestMapping("/user")
 @RestController
+@RequestMapping("/user")
 public class UserController {
-@Autowired
+    @Autowired
     UserService myservice;
-        @PostMapping("/adduser/")
-        ResponseEntity<Boolean> adddata(@RequestBody User myuserdata){
-            try{
-                myservice.saveData(myuserdata);
-                return ResponseEntity.status(HttpStatus.CREATED).body(true);
-            } catch (Exception e) {
-                return ResponseEntity.badRequest().body(false);
-            }
+
+    @PostMapping("/adduser")
+    public ResponseEntity<Boolean> adddata(@RequestBody User myuserdata) {
+        try {
+            myservice.saveData(myuserdata);
+            return ResponseEntity.status(HttpStatus.CREATED).body(true);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(false);
         }
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<User>> getalluser() {
+        try {
+            List<User> data = myservice.getalluserse();
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(data);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
 }
--- src/main/java/Service/UserService.java
+++ src/main/java/Service/UserService.java
@@ -5,9 +5,11 @@ import Entity.User;
 import Repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
-
+@Service
 public class UserService {
     @Autowired
     UserRepo myuserrepo;
@@ -15,6 +17,9 @@ public class UserService {
 
     public void saveData(User data) {
         myuserrepo.save(data);
+    }
+    public List<User> getalluserse(){
+        return myuserrepo.findAll();
     }
 
 }
